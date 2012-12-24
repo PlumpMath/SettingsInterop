33f1bd0f6b016b136cc551615d4df5d0965a6f93
FrancoisLaptopDell <FrancoisLaptopDell@48d51c72-aaca-914b-86fd-57ec478613d5>
2012-12-24 10:24:57 +0000

    git-svn-id: svn://localhost/settingsinterop/trunk@2 48d51c72-aaca-914b-86fd-57ec478613d5

---
+++ SettingsInterop/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace SettingsInterop
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			System.Windows.Forms.Application.EnableVisualStyles();
+
+			base.OnStartup(e);
+
+			SharedClasses.AutoUpdating.CheckForUpdates_ExceptionHandler();
+			//SharedClasses.AutoUpdating.CheckForUpdates(null, null, true);
+		}
+	}
+}
---
+++ SettingsInterop/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SharedClasses;
+using System.Reflection;
+
+namespace SettingsInterop
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		PropertiesEditor pe;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void ShowSettingsEditor()
+		{
+			List<object> objList;
+			Dictionary<IInterceptorNotifiable, Dictionary<PropertyInfo, object>> objectsAndPropertyValues;
+			if (!SettingsSimple.GetListOfOnlineSettings(out objList, out objectsAndPropertyValues))
+				return;//Exit if could not get list
+
+			if (pe == null)
+				pe = new PropertiesEditor(objList);
+			else
+				pe.PopulateList(objList);
+			pe.ShowDialog();
+			pe = null;
+			objList.Clear();
+			objList = null;
+
+			SettingsSimple.ProcessPropertyCompareToPrevious(objectsAndPropertyValues);
+		}
+
+		private void OnNotificationArayIconMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			if (pe != null && pe.IsVisible)
+				pe.Close();
+			else
+				ShowSettingsEditor();
+		}
+
+		private void OnMenuItemShowClick(object sender, EventArgs e)
+		{
+			ShowSettingsEditor();
+		}
+
+		private void OnMenuItemExitClick(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
